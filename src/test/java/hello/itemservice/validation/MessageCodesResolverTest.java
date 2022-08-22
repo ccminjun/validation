ef546731695caaf6dccd777d2c5298c730175e47
Minjun Choi <mi478@naver.com>
2022-08-22 23:23:59 +0900

    2022-08-22 벨리데이션 커밋

--- src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.validation.DefaultMessageCodesResolver;
 import org.springframework.validation.MessageCodesResolver;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MessageCodesResolverTest {
 
     MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
@@ -15,7 +17,8 @@ public class MessageCodesResolverTest {
         for (String messageCode : messageCodes) {
             System.out.println("messageCode = " + messageCode);
         }
-        Assertions.assertThat(messageCodes).containsExactly("required,item", "required");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+
     }
 
     @Test
@@ -24,6 +27,13 @@ public class MessageCodesResolverTest {
         for (String messageCode : messageCodes) {
             System.out.println("messageCode = " + messageCode);
         }
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+        //bindingResult.rejectValue("itemName", "required"); 이걸 하는거임
 
     }
 }
